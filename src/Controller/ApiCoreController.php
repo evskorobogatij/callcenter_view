3088b04255ae1cd0647254ba0eabe6de8095a265
Скоробогатый Евгений <evskorobogatij@gmail.com>
2020-09-10 09:08:21 +0300

    Список звонков и список операторов пропускающих звонки

---
+++ src/Controller/ApiCoreController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CallsRepository;
+use Doctrine\DBAL\Connection;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ApiCoreController
+ * @package App\Controller
+ * @Route("/api")
+ */
+class ApiCoreController extends AbstractController
+{
+
+    /**
+     * @Route("/calls_list/{date}")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function call_list($date, CallsRepository $callsRepository, Connection $connection):JsonResponse{
+
+        $data = $callsRepository->calls_list($date);
+
+        return $this->json($data);
+    }
+
+    /**
+     * @Route("/not_answer_agents/{date}")
+     * @param $date
+     * @param CallsRepository $callsRepository
+     * @return JsonResponse
+     */
+    public function not_answer_agents($date,CallsRepository $callsRepository):JsonResponse{
+        $data = $callsRepository->not_answer($date);
+        return $this->json($data);
+    }
+
+}
---
+++ src/Repository/CallsRepository.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace App\Repository;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\Persistence\ManagerRegistry;
+
+class CallsRepository
+{
+    /**
+     * @var Connection
+     */
+    private $conn;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->conn = $registry->getConnection();
+    }
+
+    function calls_list($date){
+        $str = "      
+             SELECT
+                agentdump.num as agentdump_num,
+                input_call.time, input_call.callid, 
+                enter.data2 as phone,
+                input_call.queuename, input_call.data1 as input_phone,
+                 enter.data3 as pos,
+                CASE
+                    WHEN c_agent.data1 is not null then 'Оператор положил трубку'
+                    WHEN c_caller.data1 is not null then 'Абонент положил трубку'
+                    WHEN abandon.data1 is not null then 'Пропушеный вызов'
+                end as d_type,
+               
+                t_connect.agent, 
+                -- COALESCE(c_agent.agent,c_caller.agent) as c_agent,
+                -- COALESCE(c_agent.data1, c_caller.data1) as hold_time, 
+            
+                -- t_connect.data1 as hold_time1,
+                --  abandon.data3 as wait_time,
+            
+                 COALESCE(t_connect.data1, abandon.data3) as wait,
+                COALESCE(c_agent.data2, c_caller.data2) as calltime
+                
+            FROM queue_log as input_call
+            LEFT JOIN queue_log as enter on(enter.callid=input_call.callid 
+                        -- and date_format(enter.time,'%Y-%m-%d')='2020-09-09' 
+                                         and enter.event='ENTERQUEUE')
+            LEFT JOIN queue_log as t_connect on(t_connect.callid=input_call.callid
+                                                and t_connect.event='CONNECT'
+                                               )
+            LEFT JOIN queue_log as abandon on(abandon.callid=input_call.callid
+                                            and abandon.event='ABANDON')
+            LEFT JOIN queue_log as c_agent on(c_agent.callid=input_call.callid 
+                        -- and date_format(c_agent.time,'%Y-%m-%d')='2020-09-09' 
+                                         and c_agent.event='COMPLETEAGENT')
+            LEFT JOIN queue_log as c_caller on(c_caller.callid=input_call.callid 
+                        -- and date_format(c_caller.time,'%Y-%m-%d')='2020-09-09' 
+                                         and c_caller.event='COMPLETECALLER')    
+            
+            LEFT JOIN (select callid, count(1) as num
+                    from  queue_log where  event='AGENTDUMP' 
+                    group by callid ) as agentdump on (agentdump.callid=input_call.callid)
+            where 
+                date_format(input_call.time,'%Y-%m-%d')=? 
+                and input_call.event='DID'
+            
+            order by input_call.time desc   ";
+
+        $t = $this->conn->fetchAll($str,[$date]);
+        return $t;
+    }
+
+    function not_answer($date){
+        $str = "select agent, count(1)  from  queue_log where date_format(time,'%Y-%m-%d')=? and event='RINGNOANSWER' group by agent ";
+        $d = $this->conn->fetchAll($str,[$date]);
+        return $d;
+    }
+
+}
