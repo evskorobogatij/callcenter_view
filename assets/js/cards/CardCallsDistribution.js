93931a33a4b955394749683b47f3c15b6dae618e
Скоробогатый Евгений <evskorobogatij@gmail.com>
2020-10-02 11:18:45 +0300

    Исправление подергивания графика при выбраной текущей дате

--- assets/js/cards/CardCallsDistribution.js
+++ assets/js/cards/CardCallsDistribution.js
@@ -1,41 +1,54 @@
-import React from "react";
+import React, {useMemo, useState} from "react";
 import {Card} from "primereact/components/card/Card";
 import {Chart} from "primereact/components/chart/Chart";
 import moment from "moment";
 
 function CardCalsDistribution(props) {
 
-    const [labels,setLabels] = React.useState([])
-    const [data,setData] = React.useState([])
+    const [graphData,setGraphData] = useState({
+        data : [],
+        labels: []
+    })
 
     React.useEffect(()=>{
         let s_date = moment(props.date).format("YYYY-MM-DD");
         fetch(`/api/day_distribution/${s_date}`).then(response => response.json()).then(
             result => {
-                let d = []
-                let v= []
+                let descr = []
+                let values= []
                 result.map((item)=>{
-                    d.push(item.hour+' ч')
-                    v.push(item.count)
+                    descr.push(item.hour+' ч')
+                    values.push(item.count)
                     //setLabels([...labels,item.hour+' ч'])
                 })
-                setLabels(d)
-                setData(v)
+                setGraphData({
+                    data: values,
+                    labels: descr
+                })
             }
-        ).then(()=>console.log('labels=',labels))
-        // fetch(`/api/day_distribution/${s_date}`).then(response => console.log(response))
+        )
     },[props.date])
 
-    const basicData = {
-        labels: labels,
-        datasets: [
-            {
-                label: 'Поступившие звонки',
-                backgroundColor: '#FFA726',
-                data: data
-            }
-        ]
-    };
+    const testData = React.useMemo(()=>{
+        return {
+            label : [],
+            data : []
+        }
+    })
+
+    const basicData = useMemo(()=>(
+         {
+            labels: graphData.labels,
+            datasets: [
+                {
+                    label: 'Поступившие звонки',
+                    backgroundColor: '#FFA726',
+                    data: graphData.data
+                }
+            ]
+        }
+    ),[graphData])
+
 
     let basicOptions = {
         legend: {
